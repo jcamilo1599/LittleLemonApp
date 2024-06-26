61e429f7f15ee89dbc0b363906222c593624767e
Juan Camilo <99774186+jcamilo1599@users.noreply.github.com>
2024-06-26 13:43:50 -0500

    Initial commit

---
+++ LittleLemonApp/Views/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  LittleLemonApp
+//
+//  Created by Juan Camilo Marín Ochoa on 26.06.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
